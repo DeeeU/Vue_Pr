0be765a167f2418ce5697f57d47e704bc77e3b9e
Muragaruhae <jac8for@gmail.com>
2020-07-16 02:17:12 +0900

    updata

---
+++ slot_scop/scope.js
@@ -0,0 +1,51 @@
+var TodoList = {
+  props: {
+    todos: {
+      type: Array,
+      required: true
+    }
+  },
+  template: `
+  <ul>
+    <template v-for="todo in todos">
+      <slot :todo="todo">
+        <li :key="todo.id">
+          {{ todo.text }}
+        </li>
+      </slot>
+    </template>
+  </ul>
+  `
+}
+
+new Vue({
+  el: '#app',
+  data: function () {
+    return {
+      todos: [{
+          id: 1,
+          text: 'C++',
+          isCompleted: true
+        },
+        {
+          id: 2,
+          text: 'Python',
+          isCompleted: false
+        },
+        {
+          id: 3,
+          text: 'Java',
+          isCompleted: true
+        },
+        {
+          id: 4,
+          text: 'PHP',
+          isCompleted: false
+        }
+      ]
+    }
+  },
+  components: {
+    TodoList: TodoList,
+  }
+})
