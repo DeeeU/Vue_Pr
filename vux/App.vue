ce87cedc17cbbfc4a21842ba215e4902b503ef52
Muragaruhae <jac8for@gmail.com>
2020-08-11 02:05:27 +0900

    add filter

--- vux/App.vue
+++ vux/App.vue
@@ -28,6 +28,22 @@
     <form v-on:submit.prevent="addLabel">
       <input type="text" v-model="newLabelText" placeholder="new label" />
     </form>
+
+    <h2>フィルタ</h2>
+    <ul>
+      <li v-for="label in labels" v-bind:key="label.id">
+        <input
+          type="radio"
+          v-bind:checked="label.id === filter"
+          v-on:change="changeFilter(label.id)"
+        />
+        {{ label.text }}
+      </li>
+      <li>
+        <input type="radio" v-bind:checked="filter === null" v-on:change="changeFilter(null)" />
+        フィルタしない
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -43,9 +59,13 @@ export default {
   computed: {
     tasks() {
       return this.$store.state.tasks;
+      return this.$store.getters.fillteredTasks;
     },
     labels() {
       return this.$store.state.labels;
+    },
+    filter() {
+      return this.$store.state.filter;
     },
   },
   methods: {
@@ -70,6 +90,11 @@ export default {
     getLabelText(id) {
       const label = this.labels.filter((label) => label.id === id)[0];
       return label ? label.text : "";
+    },
+    changeFilter(labelIds) {
+      this.$store.commit("changeFilter", {
+        filter: labelId,
+      });
     },
   },
 };
