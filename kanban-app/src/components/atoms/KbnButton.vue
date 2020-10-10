7c346369ab40e0a75757de88a632303dc2fa1bcf
Muragaruhae <jac8for@gmail.com>
2020-10-10 21:41:50 +0900

    test

--- kanban-app/src/components/atoms/KbnButton.vue
+++ kanban-app/src/components/atoms/KbnButton.vue
@@ -1,5 +1,10 @@
 <template>
-  <button :class="classes" :disabled="disabled" type="button" @click="handleClick">
+  <button
+    :class="classes"
+    :disabled="disabled"
+    type="button"
+    @click="handleClick"
+  >
     <slot />
   </button>
 </template>
@@ -7,6 +12,7 @@
 <script>
 export default {
   name: "KbnButton",
+
   props: {
     type: {
       type: String,
@@ -19,6 +25,7 @@ export default {
   },
 
   computed: {
+    // `type`に応じてクラスを動的に生成する
     classes() {
       const cls = this.type === "text" ? "-" + this.type : "";
       return [`kbn-button${cls}`];
@@ -26,6 +33,7 @@ export default {
   },
 
   methods: {
+    // `click`イベントを発行
     handleClick(ev) {
       this.$emit("click", ev);
     },
--- kanban-app/src/router/index.js
+++ kanban-app/src/router/index.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import routes from './routes'
+import {
+  authorizeToken
+} from './guards'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes
 })
+router.beforeEach(authorizeToken)
+
+export default router
